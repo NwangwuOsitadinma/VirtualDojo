73627035b04eb302a0347d0b56fae46803cdaf0a
Ositadinma Nwangwu <Ositadinma Nwangwu>
2018-02-26 08:40:35 +0100

    added message handler

---
+++ Utils/MessageHandler.js
@@ -0,0 +1,36 @@
+exports.resourceNotFound = function() {
+    return 'The resource you requested was not found. Please try again'
+}
+exports.resourceFound = function() {
+    return 'The resource was retrieved successfully'
+}
+exports.success = function() {
+    return 'The request was successful';
+}
+exports.failed = function() {
+    return 'Something went wrong and the request was not successful';
+}
+
+exports.failedToUpdate = function() {
+    return 'Something went wrong and the resource was not updated successfully';
+}
+
+exports.updateSuccessful = function() {
+    return 'The resource was updated successfully';
+}
+
+exports.failedToDelete = function() {
+    return 'Something went wrong and the resource could not be deleted';
+}
+
+exports.deleteSuccessful = function() {
+    return 'The resource was deleted successfully';
+}
+
+exports.createdSuccessful = function() {
+    return 'The resource was created successfully';
+}
+
+exports.failedToCreate = function() {
+    return 'The resource was created successfully';
+}
